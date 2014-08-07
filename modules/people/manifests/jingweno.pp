1fe44217def0c18d3a67cdb3f020cc2a8aa4fdb4
Owen <owen@Owens-MacBook-Pro.local>
2014-08-07 00:03:04 -0700

    Add my settings

---
+++ modules/people/manifests/jingweno.pp
@@ -0,0 +1,27 @@
+class people::jingweno {
+  include projects::all
+
+  package {
+    'coreutils':
+      ensure => latest;
+    'vim':
+      ensure => latest;
+    'tmux':
+      ensure => latest;
+    'zsh-completions':
+      ensure => latest;
+    'hg':
+      ensure => latest;
+    'reattach-to-user-namespace':
+      ensure => latest;
+  }
+
+  $home       = "/Users/${::luser}"
+  $dotfiles   = "${home}/.dotfiles"
+
+  repository {
+    $dotfiles:
+      source => 'jingweno/dotfiles',
+      path   => $dotfiles;
+  }
+}
